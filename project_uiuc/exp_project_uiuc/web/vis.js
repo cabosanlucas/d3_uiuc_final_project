110135d106ab916a7154600422bd78f592b1377f
cabosanlucas <kaju99@gmail.com>
2017-04-17 14:43:04 -0500

    trying to get map to work

--- project_uiuc/exp_project_uiuc/web/vis.js
+++ project_uiuc/exp_project_uiuc/web/vis.js
@@ -2,7 +2,7 @@
 
 /* Boilerplate jQuery */
 $(function() {
-  $.get("res/illiniFootballScores_streak.csv")
+  $.get("res/stateslived.csv")
    .done(function (csvData) {
      var data = d3.csvParse(csvData);
      visualize(data);
@@ -33,4 +33,40 @@ var visualize = function(data) {
 
 
   // == Your code! :) ==
+  // D3 Projection
+var projection = d3.geo.albersUsa()
+           .translate([width/2, height/2])    // translate to center of screen
+           .scale([1000]);          // scale things down so see entire US
+        
+// Define path generator
+var path = d3.geo.path()               // path generator that will convert GeoJSON to SVG paths
+         .projection(projection);  // tell path generator to use albersUsa projection
+
+    
+// Define linear scale for output
+var color = d3.scale.linear()
+        .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
+
+
+  // Bind the data to the SVG and create one path per GeoJSON feature
+svg.selectAll("path")
+  .data(json.features)
+  .enter()
+  .append("path")
+  .attr("d", path)
+  .style("stroke", "#fff")
+  .style("stroke-width", "1")
+  .style("fill", function(d) {
+
+  // Get data value
+  var value = d.properties.visited;
+
+  if (value) {
+  //If value exists…
+  return color(value);
+  } else {
+  //If value is undefined…
+  return "rgb(213,222,217)";
+  }
+});
 };
